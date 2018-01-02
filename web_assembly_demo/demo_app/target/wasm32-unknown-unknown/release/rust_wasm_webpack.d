f001dd44f0d9a93fea0c0d1396b8ad75618cfe0a
Toni Dezman <programmertoni@gmail.com>
2018-01-02 04:28:04 +0100

    changes to the slides

--- web_assembly_demo/demo_app/target/wasm32-unknown-unknown/release/rust_wasm_webpack.d
+++ web_assembly_demo/demo_app/target/wasm32-unknown-unknown/release/rust_wasm_webpack.d
@@ -1,1 +1,1 @@
-/Users/tonid/apps/Talks/web_assembly_demo/rust-wasm-webpack/target/wasm32-unknown-unknown/release/rust_wasm_webpack.wasm: /Users/tonid/apps/Talks/web_assembly_demo/rust-wasm-webpack/src/lib.rs
+/Users/tonid/apps/Talks/web_assembly_demo/demo_app/target/wasm32-unknown-unknown/release/rust_wasm_webpack.wasm: /Users/tonid/apps/Talks/web_assembly_demo/demo_app/src/lib.rs
